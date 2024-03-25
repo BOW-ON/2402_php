c1b43d6fec7220396e4aeac67881c5c1611cb3d5
BOW-ON <qhdnjs94@gmail.com>
2024-03-25 17:39:55 +0900

    240325 ~ 프로젝트 연습 예제

---
+++ php/side_project/mini_board/src/header.php
@@ -0,0 +1,41 @@
+<div class="snowflakes" aria-hidden="true">
+    <div class="snowflake">
+        <div class="inner"><img src="./3333.png" alt="꽃"></div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+    <div class="snowflake">
+        <div class="inner">*</div>
+    </div>
+</div>
+<header>
+    <h1><a href="./list.php">mini Board</a></h1>
+</header>
