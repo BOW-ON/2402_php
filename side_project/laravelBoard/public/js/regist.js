731ad53cb69ed80be6b0d6791dd8811d4e592eee
BOW-ON <qhdnjs94@gmail.com>
2024-05-16 10:51:41 +0900

    240516 라라벨 ~회원가입 권한체크 진행중

--- side_project/laravelBoard/public/js/regist.js
+++ side_project/laravelBoard/public/js/regist.js
@@ -1,12 +1,133 @@
 // email 검색후 axios 처리
-document.querySelector('#btn-chk-email').addEventListener('click', chkEmailPassword);
+document.querySelector('#btn-chk-email').addEventListener('click', chkEmail);
+let registChk = {
+    'emailChk': false
+    ,'passwordChk': false
+    ,'nameChk': false
+};
 
-// async로 작성
-async function chkEmailPassword() {
+// 이메일 체크(async로 작성)
+async function chkEmail() {
     
     try{
         const email = document.querySelector('#email').value;
+
+        const url = '/user/chk';
+    
+    
+        // 폼 데이터 생성
+        const form = new FormData();
+        form.append('email', email);
+    
+        // Ajax
+        const response = await axios.post(url, form);
+
+        const btnComplete = document.querySelector('#my-btn-complete');
+        const printChkEmail = document.querySelector('#print-chk-email');
+        printChkEmail.innerHTML = '';
+        // 정상 처리
+        if(response.data.emailFlg) {
+            // 중복 이메일
+            printChkEmail.innerHTML = '사용불가';
+            printChkEmail.classList = 'text-danger fw-bold';
+            btnComplete.setAttribute('disabled', 'disabled');
+        } else {
+            // 사용 가능 이메일
+            printChkEmail.innerHTML = '사용가능';
+            printChkEmail.classList = 'text-success fw-bold';
+            btnComplete.removeAttribute('disabled');
+        }
+
+        // TODO : chkRegist 추가
+
+    } catch(err) {
+        console.log(err);
+        alert('회원가입 실패')
+    }
+
+}
+
+// 이름 체크
+async function chkName() {
+    try{
+        const name = document.querySelector('#name').value;
+
+        const url = '/user/chk';
+    
+    
+        // 폼 데이터 생성
+        const form = new FormData();
+        form.append('name', name);
+    
+        // Ajax
+        const response = await axios.post(url, form);
+
+        const btnComplete = document.querySelector('#my-btn-complete');
+        const printChkEmail = document.querySelector('#print-chk-email');
+        printChkEmail.innerHTML = '';
+        // 정상 처리
+        if(response.data.emailFlg) {
+            // 중복 이메일
+            printChkEmail.innerHTML = '사용불가';
+            printChkEmail.classList = 'text-danger fw-bold';
+            btnComplete.setAttribute('disabled', 'disabled');
+        } else {
+            // 사용 가능 이메일
+            printChkEmail.innerHTML = '사용가능';
+            printChkEmail.classList = 'text-success fw-bold';
+            btnComplete.removeAttribute('disabled');
+        }
+
+        // TODO : chkRegist 추가
+
+    } catch(err) {
+        console.log(err);
+        alert('회원가입 실패')
+    }
+}
+
+// 패스워드 체크
+async function chkPassword() {
+    try{
         const password = document.querySelector('#password').value;
+
+        const url = '/user/chk';
+    
+    
+        // 폼 데이터 생성
+        const form = new FormData();
+        form.append('email', email);
+    
+        // Ajax
+        const response = await axios.post(url, form);
+
+        const btnComplete = document.querySelector('#my-btn-complete');
+        const printChkEmail = document.querySelector('#print-chk-email');
+        printChkEmail.innerHTML = '';
+        // 정상 처리
+        if(response.data.emailFlg) {
+            // 중복 이메일
+            printChkEmail.innerHTML = '사용불가';
+            printChkEmail.classList = 'text-danger fw-bold';
+            btnComplete.setAttribute('disabled', 'disabled');
+        } else {
+            // 사용 가능 이메일
+            printChkEmail.innerHTML = '사용가능';
+            printChkEmail.classList = 'text-success fw-bold';
+            btnComplete.removeAttribute('disabled');
+        }
+
+        // TODO : chkRegist 추가
+
+    } catch(err) {
+        console.log(err);
+        alert('회원가입 실패')
+    }
+}
+
+// 패스워드 확인 체크
+async function chkPasswordChk() {
+    try{
         const password2 = document.querySelector('#password-chk').value;
 
         const url = '/user/chk';
@@ -35,10 +156,46 @@ async function chkEmailPassword() {
             btnComplete.removeAttribute('disabled');
         }
 
+        // TODO : chkRegist 추가
+
     } catch(err) {
         console.log(err);
         alert('회원가입 실패')
     }
-
 }
 
+
+// 이메일, 패스워드, 패스워드 확인, 이름이 모두 확인 되면 버튼 활성화
+async function chkRegist() {
+    try{
+
+        // 폼 데이터 생성
+        const form = new FormData();
+        form.append('email', email);
+    
+        // Ajax
+        const response = await axios.post(url, form);
+
+        const btnComplete = document.querySelector('#my-btn-complete');
+        const printChkEmail = document.querySelector('#print-chk-email');
+        printChkEmail.innerHTML = '';
+        // 정상 처리
+        if(response.data.emailFlg) {
+            // 중복 이메일
+            printChkEmail.innerHTML = '사용불가';
+            printChkEmail.classList = 'text-danger fw-bold';
+            btnComplete.setAttribute('disabled', 'disabled');
+        } else {
+            // 사용 가능 이메일
+            printChkEmail.innerHTML = '사용가능';
+            printChkEmail.classList = 'text-success fw-bold';
+            btnComplete.removeAttribute('disabled');
+        }
+
+        // TODO : chkRegist 추가
+
+    } catch(err) {
+        console.log(err);
+        alert('회원가입 실패')
+    }
+}
--- side_project/laravelBoard/resources/views/regist.blade.php
+++ side_project/laravelBoard/resources/views/regist.blade.php
@@ -34,14 +34,28 @@
     <button id="btn-chk-email" type="button" class="btn btn-secondary float-end mb-1">중복 확인</button>
     <input type="text" class="form-control mb-3" id="email" name="email">
 
+    <label for="name" class="form-label">이름</label>
+    <input type="text" class="form-control mb-3" id="name" name="name">
+
     <label for="password" class="form-label">비밀번호</label>
     <input type="password" class="form-control mb-3" id="password" name="password">
 
     <label for="password-chk" class="form-label">비밀번호 확인</label>
     <input type="password" class="form-control mb-3" id="password-chk" name="password-chk">
 
-    <label for="name" class="form-label">이름</label>
-    <input type="text" class="form-control mb-3" id="name" name="name">
+    {{-- oninput : 입력값을 받아 실시간으로 처리 --}}
+    <input type="text" oninput="handleInput(event)">
+
+    <script>
+        function handleInput(event) {
+            console.log('Input value:', event.target.value);
+            if (event.target.value === '1') {
+              return 'oninput 테스트'
+            } else {
+              return 'oninput 테스트2'
+            }
+        }
+    </script>
 
     <button id="my-btn-complete" type="submit" class="btn btn-dark">완료</button>
     {{-- <button id="my-btn-complete" type="submit" disabled="disabled" class="btn btn-dark">완료</button> --}}
